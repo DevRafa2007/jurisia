2675e966e5e3bd7342df653b07ceed40e488b6a4
Rafael <rafafaelloso2007@gmail.com>
2025-03-21 15:48:30 -0300

    Adicionar dica sobre exportação para DOCX e edição no Word/Google Docs

--- pages/documentos.tsx
+++ pages/documentos.tsx
@@ -1148,6 +1148,19 @@ ${camposDoc.map(campo => {
         </div>
       </div>
 
+      {/* Dica para o usuário */}
+      <div className="mb-4 p-3 bg-blue-50 dark:bg-blue-900/30 border border-blue-200 dark:border-blue-800 rounded-md text-sm text-blue-700 dark:text-blue-300 no-print">
+        <div className="flex items-start">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 flex-shrink-0 text-blue-600 dark:text-blue-400" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9z" clipRule="evenodd" />
+          </svg>
+          <div>
+            <p className="font-medium">Dica para melhor formatação:</p>
+            <p>Para edição avançada do documento, recomendamos baixar como DOCX e utilizar o Microsoft Word ou Google Docs para ajustes finais na formatação e estilo.</p>
+          </div>
+        </div>
+      </div>
+
       {/* TinyMCE Editor */}
       <div className="bg-white shadow-lg mx-auto rounded-sm overflow-hidden print:shadow-none mb-10 w-full">
         <Editor
